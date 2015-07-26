07ea4604a267fecf4af50fcfdd947d73bf68cc8b
jan hanson <jpchansondev@gmail.com>
2015-07-26 15:08:14 -0400

    wordpress theme

---
+++ DBell/header.php
@@ -0,0 +1,35 @@
+<!--__________________________ HEADER _____________________________-->
+
+<!--__________________________ DOCTYPE ____________________________-->
+<!DOCTYPE html>
+<!-- _______________________ENDOF DOCTYPE _________________________-->
+
+
+<html lang="en">
+
+<!--___________________________ HEAD ______________________________-->  
+
+<head>
+  	<title><?php bloginfo('name');?></title>
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <!-- JQuery library -->
+	<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+  
+  <!-- JQuery mobile library and CSS -->
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+	<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+
+  <!-- link to worpress style.css -->
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>" type="text/css" media="screen"></link>
+
+</head>
+
+<!--_________________________ ENDOF HEAD ____________________________-->
+
+  
+  
+<body>
+<!--________________________ ENDOF HEADER ___________________________-->
+  
+  
---
+++ DBell/index.php
@@ -0,0 +1,49 @@
+<?php get_header(); ?>
+
+
+<!-- __________________________CONTENT AREA__________________________-->
+ 
+<!-- STARTOF POST LIST  -->
+<?php if ( have_posts() ) :?>
+		<!-- STARTOF MAIN LOOP -->
+		<?php while ( have_posts() ) : the_post();?>
+
+
+<!--______________________________ POST _____________________________-->
+
+
+					<!-- post title and link -->
+					<h2 ><a href="<?php the_permalink()?>"><?php the_title();?></a></h2>
+				 
+					<!-- thumbnail and link -->  
+			 		<?php if(has_post_thumbnail()):?>
+			  			<div>
+				  			<a href="<?php the_permalink()?>"><?php the_post_thumbnail();?></a>
+			  			</div>
+			 		<?php endif;?>
+				 	<!-- endof thumbnail and link -->
+				 
+			 	 	<!-- the content of the post -->
+			   	<?php the_excerpt(); ?>
+
+					
+
+			<?php /*** insert page format footer ***/?>
+ 			<?php get_template_part('page_format_footer');?>
+<!--____________________________ ENDOF POST __________________________-->
+
+
+		<?php endwhile;?>
+		<!-- ENODF MAIN LOOP -->		
+
+<?php else :?>
+	<h2>Sorry No Posts Found </h2>
+	<p>Try searching for something else </p>
+	<p><a href=<?php echo get_option('home');?>>Return to Homepage</a></p>
+
+<?php endif;?>
+<!-- ENDOF POST LIST  -->
+
+<!-- _______________________ENDOF CONTENT AREA______________________ --> 
+
+<?php get_footer(); ?>
